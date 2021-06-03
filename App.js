46aa13797ad985eaa51ea9c448969583e8fd8f0a
J.S <pachipico1@gmail.com>
2021-06-04 01:25:21 +0900

    Use RN made simple to-do-list, not done yet

--- App.js
+++ App.js
@@ -1,21 +1,103 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Platform } from "react-native";
+import styled from "styled-components/native";
+import Constants from "expo-constants";
+import _ from "lodash";
+
+const Container = styled.SafeAreaView`
+	border: 1px solid #ff0000;
+	padding-top: ${Constants.statusBarHeight}px;
+	flex: 1;
+`;
+
+const Contents = styled.ScrollView`
+	flex: 1;
+	border: 1px solid red;
+`;
+
+const KeyboardAvoidingView = styled.KeyboardAvoidingView`
+	flex: 1;
+`;
+
+const Input = styled.TextInput`
+	border: 1px solid #e5e5e5;
+	padding: 10px 24px;
+	flex: 1;
+`;
+
+const ToDoItem = styled.View`
+	align-items: center;
+	flex-direction: row;
+`;
+
+const ToDoText = styled.Text`
+	font-size: 20px;
+	padding: 8px 24px;
+
+	flex: 1;
+`;
+
+const ToDoItemButton = styled.Button``;
+
+const InputContainer = styled.View`
+	flex-direction: row;
+`;
+
+const Button = styled.Button``;
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+	const [newTodo, setNewTodo] = useState("");
+	const [list, setList] = useState([
+		{
+			id: "1",
+			todo: "할일1",
+		},
+		{
+			id: "2",
+			todo: "할일2",
+		},
+	]);
+	return (
+		<Container>
+			<KeyboardAvoidingView
+				behavior={Platform.OS === "ios" ? "padding" : "height"}
+			>
+				<Contents>
+					{list.map((item) => {
+						return (
+							<ToDoItem key={item.id}>
+								<ToDoText>{item.todo}</ToDoText>
+								<ToDoItemButton
+									title='삭제'
+									onPress={() => {
+										const rejected = _.reject(
+											list,
+											(garbage) => item.id === garbage.id
+										);
+										setList(rejected);
+									}}
+								/>
+							</ToDoItem>
+						);
+					})}
+				</Contents>
+				<InputContainer>
+					<Input onChangeText={(text) => setNewTodo(text)} value={newTodo} />
+					<Button
+						title='전송'
+						onPress={() => {
+							if (newTodo.replace(/(\s*)/g, "") === "") {
+								setNewTodo("");
+								return;
+							}
+							setList(() => [...list, { id: list.length + 1, todo: newTodo }]);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+							setNewTodo("");
+						}}
+					></Button>
+				</InputContainer>
+			</KeyboardAvoidingView>
+		</Container>
+	);
+}
